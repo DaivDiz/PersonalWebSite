8e414a4777ac885b19e030fb3b4c3a1bf46d98a6
David Diaz <daivdiz@outlook.com>
2021-01-14 09:17:08 -0500

    Intento de orden 3 - En proceso - Primera Critica - Verde

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -33,8 +33,8 @@
                     <div class="pp">
                         <p>Tengo 18 años y soy desarrollador Full-Stack enfocado más al desarrollo
                         Front-End, cuento con solo experiencia personal desarrollando sitios web. Actualmente
-                        soy estudiante activo de <strong>Ingeniería Mecatrónica</strong> en la <strong>Universidad de San Buenaventura 
-                        - Sede Bogota</strong> y estudiante activo en la plataforma de aprendizaje virtual 
+                        soy estudiante activo de <strong class="subRayado"><a href="https://www.usbbog.edu.co/" class="txtColor" target="_blank">
+                        Ingeniería Mecatrónica</a></strong> en la Universidad de<strong> San Buenaventura - Sede Bogota</strong> y de la plataforma de aprendizaje virtual 
                         <strong class="subRayado"><a href="https://platzi.com/p/DaivDiz/" target="_blank" class="txtColor">Platzi</a></strong>,
                         tomando las clases de la <strong>Escuela de Desarrollo Web</strong> enfocandola a Front-End sin
                         dejar de lado el Back-End y las Bases de Datos.</p>
@@ -177,5 +177,348 @@ export default {
 .iconList-leave, .iconList-enter-to{
     /* transform: translateY(calc(0px + 20 * (100vw - 320px) / 1080)); */
     opacity: 1;
+}
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;200;300;400;500;600;700&display=swap');
+::-webkit-scrollbar {
+    width: 4px;
+}
+
+::-webkit-scrollbar-track {
+    background: transparent;
+}
+
+::-webkit-scrollbar-thumb {
+    border-radius: 5px 0 0 5px;
+}
+
+.txtColor,
+.subRayado,
+.com,
+::-webkit-scrollbar-thumb,
+::-webkit-scrollbar-thumb:hover {
+    background: linear-gradient(320deg, #DCE35B, #45B649);
+}
+
+* {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+}
+
+html {
+    overflow-y: overlay;
+}
+
+div {
+    font-size: inherit;
+    color: inherit;
+    background: transparent;
+}
+
+.bn {
+    transition: opacity .2s;
+    cursor: pointer;
+    width: calc(55px + 7 * (100vw - 320px) / 1080);
+    height: calc(55px + 7 * (100vw - 320px) / 1080);
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+}
+
+.bn>div {
+    width: calc(22px + 4 * (100vw - 320px) / 1080);
+    height: 10px;
+    position: relative;
+}
+
+.bn>div>span {
+    transition: background .4s;
+    position: absolute;
+    width: 100%;
+    height: 2px;
+    background-color: #edf1f3;
+}
+
+.bn>div>span:nth-child(1) {
+    /* top: 0; */
+    /* transition: background .4s, top .2s .2s, transform .2s; */
+    transition: transform .3s, top .8s .8s;
+    animation: clickTop .5s ease-in-out forwards reverse;
+    transform: rotate(0);
+}
+
+.bn>div>span:nth-child(2) {
+    /* bottom: 0; */
+    /* transition: background .4s, bottom .2s .2s, transform .2s; */
+    transition: transform .3s, bottom .8s .8s;
+    animation: clickBottom .5s ease-in-out forwards reverse;
+    transform: rotate(0);
+}
+
+.bn:active span:nth-child(1) {
+    animation: clickTop .5s ease-in-out forwards;
+    transform: rotate(45deg);
+}
+
+.bn:active span:nth-child(2) {
+    animation: clickBottom .5s ease-in-out forwards;
+    transform: rotate(-45deg);
+}
+
+@keyframes clickTop {
+    0% {
+        top: 0;
+    }
+    90%,
+    100% {
+        top: 4px;
+    }
+}
+
+@keyframes clickBottom {
+    0% {
+        bottom: 0;
+    }
+    90%,
+    100% {
+        bottom: 4px;
+    }
+}
+
+.bn svg {
+    stroke: #3f00e6;
+    mix-blend-mode: normal;
+    stroke-linecap: round;
+    transition: all .8s, opacity 0s .8s;
+    stroke-dasharray: 0 999;
+    fill: transparent;
+    stroke-width: 4px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    opacity: 0;
+}
+
+.bn:hover svg {
+    stroke-dasharray: 300 999;
+    opacity: 1;
+    transition: all .8s, opacity 0s;
+    transform: rotate(90deg);
+}
+
+.sc .ct .pp {
+    width: calc(900px + 100 * (100vw - 320px) / 1080);
+    max-width: 100%;
+    line-height: 1.5;
+    padding-top: 15px;
+    margin: calc(10px + 1.5%) 0 calc(10px + 1%);
+}
+
+.sc .ct .pp p {
+    margin: calc(10px + 1.5%) 0 calc(10px + 1%);
+}
+
+strong {
+    font-weight: bold;
+}
+
+.sc .ct .intro {
+    font-size: calc(14px + 5 * (100vw - 320px) / 1080);
+}
+
+header {
+    position: absolute;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: calc(15px + 2%) calc(15px + 2%) calc(15px + 2%) calc(25px + 2%);
+    z-index: 9999;
+}
+
+.izqLogo a {
+    text-decoration: none;
+    font-family: 'Josefin Sans', sans-serif;
+    font-weight: 600;
+    font-size: calc(35px + 5 * (100vw - 320px) / 1080);
+    color: white;
+}
+
+.txtColor,
+.txtColor2 {
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+}
+
+.txtColor2 {
+    background-image: linear-gradient(320deg, #2193b0, #6dd5ed);
+}
+
+.subRayado {
+    background-size: 0% 2px;
+    background-position: 50% 100%;
+    background-repeat: no-repeat;
+    transition: background-size 0.3s;
+}
+
+.subRayado2,
+.sc {
+    background-image: linear-gradient(to left, #16141c, #09080b);
+}
+
+.subRayado2 {
+    background-size: 0% 2px;
+    background-position: 50% 100%;
+    background-repeat: no-repeat;
+    transition: background-size 0.3s;
+}
+
+.subRayado:hover,
+.subRayado2:hover {
+    background-size: 100% 2px;
+}
+
+.sc {
+    padding: calc(100px + 80 * (100vw - 320px) / 1080) 0;
+    font-weight: 200;
+    color: #edf1f3;
+    font-family: 'Montserrat', sans-serif;
+}
+
+.sc .ct {
+    width: 70vw;
+    margin: auto;
+}
+
+.sc .ct h2 {
+    margin-top: calc(25px + 2%);
+    font-size: calc(30px + 20 * (100vw - 320px) / 1080);
+    font-weight: 800;
+}
+
+.hspan>span {
+    line-height: 1.2;
+    overflow: hidden;
+    margin-bottom: calc(0px - 5 * (100vw - 320px) / 1080);
+}
+
+.hspan span {
+    display: block;
+    white-space: nowrap;
+}
+
+.sc h2 strong {
+    font-weight: inherit;
+}
+
+.com {
+    padding: calc(100px + 80 * (100vw - 320px) / 1080) 0;
+    font-family: 'Montserrat', sans-serif;
+    transition: transform .1s ease-out;
+    font-weight: 300;
+    color: #edf1f3;
+}
+
+.com .ct {
+    text-align: center;
+}
+
+.com .ct h2 {
+    line-height: calc(45px + 7 * (100vw - 320px) / 1080);
+}
+
+.com .ct strong {
+    font-weight: 200;
+    font-size: calc(41px + 7 * (100vw - 320px) / 1080);
+}
+
+.com .ct small {
+    font-weight: 700;
+    font-size: calc(41px + 7 * (100vw - 320px) / 1080);
+}
+
+.com .bt {
+    height: 100%;
+    margin-top: calc(50px + 7 * (100vw - 320px) / 1080);
+    text-align: center;
+}
+
+.com .bt strong {
+    user-select: none;
+    font-weight: 300;
+    font-size: calc(41px + 7 * (100vw - 320px) / 1080);
+    letter-spacing: 5px;
+}
+
+.com .iconList {
+    display: flex;
+    justify-content: space-evenly;
+    /* margin-top: calc(20px + 7 * (100vw - 320px) / 1080); */
+}
+
+.cn {
+    transition: opacity .2s;
+    cursor: pointer;
+    width: calc(55px + 7 * (100vw - 320px) / 1080);
+    height: calc(55px + 7 * (100vw - 320px) / 1080);
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+}
+
+.cn {
+    display: flex;
+}
+
+.cn>img {
+    height: calc(30px + 20 * (100vw - 320px) / 1080);
+}
+
+.cn svg {
+    stroke: green;
+    mix-blend-mode: normal;
+    stroke-linecap: round;
+    transition: all .8s, opacity 0s .8s;
+    stroke-dasharray: 0 999;
+    fill: transparent;
+    stroke-width: 4px;
+    width: 200%;
+    position: absolute;
+    top: -50%;
+    left: -50%;
+    opacity: 0;
+}
+
+.cn:hover svg {
+    stroke-dasharray: 300 999;
+    opacity: 1;
+    transition: all .8s, opacity 0s;
+    transform: rotate(90deg);
+}
+
+@media only screen and (max-width: 720px) {
+    .sc .ct h2 {
+        font-size: calc(22px + 20 * (100vw - 320px) / 1080);
+    }
+    .com .ct strong {
+        font-size: calc(28px + 7 * (100vw - 320px) / 1080);
+    }
+    .com .ct small {
+        font-size: calc(20px + 7 * (100vw - 320px) / 1080);
+    }
+    .cn svg {
+        width: 100%;
+        top: 0;
+        left: 0;
+    }
+    .cn>img {
+        width: calc(22px + 20 * (100vw - 320px) / 1080);
+    }
 }
 </style>
